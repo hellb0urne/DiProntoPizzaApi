e97bde7a78b286b9ac9912369eb2e4e75a061969
hellb0urne <robertybenja3119@gmail.com>
2023-03-30 01:59:52 -0300

    Mejorando proyecto

--- src/main/java/com/example/dipronto/diprontoDemo/properties/ProductoProperties.java
+++
@@ -1,9 +0,0 @@
-package com.example.dipronto.diprontoDemo.properties;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "activity")
-public class ProductoProperties {
-
-
-}
